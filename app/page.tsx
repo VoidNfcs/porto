a99b2b01af94d94b2d13b475b3888ebede5257db
VoidNFCs <voidnfc@gmail.com>
2024-10-04 11:18:44 +0700

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const Home = () => {
-	return (
-		<main className='flex min-h-screen flex-col bg-[#121212] '>
-			<Navbar />
-			<div className='container mt-24 mx-auto py-4 px-12'>
-				<HeroSection />
-			</div>
-		</main>
-	);
+    return (
+        <main className='flex min-h-screen flex-col bg-[#121212]'>
+            <Navbar />
+            <div className='container mt-24 mx-auto py-4 px-12'>
+                <HeroSection />
+            </div>
+        </main>
+    );
 };
 
 export default Home;
