778e27e775e1af08c10e3ba69b28c6718261e82d
voidnfc <voidnfc@gmail.com>
2024-10-04 12:32:00 +0000

    reverse grid and add shadow on the navbar

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,8 +7,8 @@ const HeroSection = () => {
     return (
         <section>
             <div className='grid grid-cols-1 sm:grid-cols-12'>
-                <div className='col-span-7 place-self-center text-center sm:text-left'>
-                    <h1 className='text-white mb-4 text-4xl sm::text-5xl lg:text-6xl font-extrabold'>
+                <div className='col-span-7 place-self-center mt-4 text-center sm:text-left sm:col-start-1 sm:col-end-8 order-2 sm:order-1'>
+                    <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
                             {"Hello, I'am"}{' '}
                         </span>
@@ -41,14 +41,14 @@ const HeroSection = () => {
                         <button className='w-full sm:w-fit px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
                             Hire Me
                         </button>
-                        <button className='w-full sm:w-fit px-1 py-1 rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3'>
+                        <button className='w-full sm:w-fit px-1 py-1 rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
                             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                                 Download CV
                             </span>
                         </button>
                     </div>
                 </div>
-                <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                <div className='col-span-5 place-self-center lg:mt-0 sm:col-start-8 sm:col-end-13 order-1 sm:order-2'>
                     <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
                         <Image
                             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[180px] h-[180px] lg:w-[300px] lg:h-[300px]'
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ const navLinks: navLinksData[] = [
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-        <nav className='fixed top-0 left-0 right-0  z-10 bg-[#121212] bg-opacity-100'>
+        <nav className='fixed top-0 left-0 right-0  z-10 bg-[#121212] bg-opacity-100 shadow-md'>
             <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link
                     href={'/'}
