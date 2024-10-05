8c266b70a35e2c1981b09a26ae1d69e90ab8cb3c
VoidNFCs <voidnfc@gmail.com>
2024-10-05 15:30:04 +0700

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         <nav className='fixed top-0 left-0 right-0  bg-[#121212] bg-opacity-100 border border-[#33353F] z-50'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link
-                    href={'/'}
+                    href={'#home'}
                     className='text-2xl md:text-5xl text-white font-semibold'
                 >
                     Portfolio
