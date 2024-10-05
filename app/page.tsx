acb15865733b1445c0a3d589bf2ba0ab8d2bfe9e
VoidNFCs <voidnfc@gmail.com>
2024-10-05 11:04:06 +0700

    pull

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,18 @@ import ProjectSection from '@/components/ProjectSection';
 import React from 'react';
 
 const Home = () => {
-	return (
-		<main className='flex min-h-screen flex-col bg-[#121212]'>
-			<Navbar />
-			<div className='container mt-24 mx-auto py-4 px-12'>
-				<HeroSection />
-				<AboutSection />
-				<ProjectSection />
-				<EmailSection />
-			</div>
-			<Footer />
-		</main>
-	);
+    return (
+        <main className='flex min-h-screen flex-col bg-[#121212]'>
+            <Navbar />
+            <div className='container mt-24 mx-auto py-4 px-12'>
+                <HeroSection />
+                <AboutSection />
+                <ProjectSection />
+                <EmailSection />
+            </div>
+            <Footer />
+        </main>
+    );
 };
 
 export default Home;
